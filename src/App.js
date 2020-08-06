7460b49a02c8b2493dc5f50a9e9a0eaf31b45658
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-05 22:27:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+
+  //1 write a handleClick function that fetches names from API.
+  // iterate through the names 
+  // display names
+
+  const [pokemon, setPokemon] = useState([]);
+
+  const handleClick = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807')
+      .then(response => setPokemon(response.data.results))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Click the button to diplay the names of all 807 Pokemon using the <a target='black' href='https://pokeapi.co/'>Pokemon API</a>!</h3>
+      <button onClick={handleClick}>Fetch Pokemon</button>
+
+      <div id="box">
+        <ul>
+          {pokemon.length > 0 && pokemon.map((pokemon, index) => {
+            // arrow function to traverse thru the pokemon json objects returned in line 
+            return (<li key={index}>{pokemon.name}</li>)
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
